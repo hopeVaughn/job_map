606cd8f909487112dededb37cc949dc8588d4d41
NadiaPia <206i417@gmail.com>
2022-11-22 14:55:16 -0800

    Form input and axios post request added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Footer } from "./components"
-import { Home, LandingPage, ListedCompanies, CompanyPage, Login, Network } from './pages';
+import { Home, LandingPage, ListedCompanies, CompanyPage, Login, Register, Network } from './pages';
 import { useState } from "react";
 
 function App() {
@@ -19,6 +19,8 @@ function App() {
         <Route path='companies' element={<ListedCompanies levelClicked={levelClicked}  />} />
         <Route path='companies/:id' element={<CompanyPage />} />
         <Route path='login' element={<Login setAuth={setAuth}/>} />
+        <Route path='register' element={<Register />} />
+
         <Route path='network' element={<Network />} />
 
       </Routes>
