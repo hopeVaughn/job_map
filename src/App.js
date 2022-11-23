9a5e42232ffb166b88d1846dbe6b718c005065bc
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-22 17:12:07 -0800

    fix stretched images

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Footer } from "./components"
 
-import { Home, LandingPage, ListedCompanies, CompanyPage, Login, Network, SingleNetwork } from './pages';
+import { Home, LandingPage, ListedCompanies, CompanyPage, Login, Register, Network, SingleNetwork } from './pages';
 
 import { useState } from "react";
 
@@ -17,10 +17,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='landing' element={<LandingPage setLevelClicked={setLevelClicked}/>} />
-        <Route path='companies' element={<ListedCompanies levelClicked={levelClicked}  />} />
+        <Route path='landing' element={<LandingPage setLevelClicked={setLevelClicked} />} />
+        <Route path='companies' element={<ListedCompanies levelClicked={levelClicked} />} />
         <Route path='companies/:id' element={<CompanyPage />} />
-        <Route path='login' element={<Login setAuth={setAuth}/>} />
+        <Route path='login' element={<Login setAuth={setAuth} />} />
         <Route path='register' element={<Register />} />
 
         <Route path='network' element={<Network />} />
--- src/components/ListAllContact.js
+++ src/components/ListAllContact.js
@@ -4,22 +4,22 @@ import Avatar from 'react-avatar';
 function ListAllContact() {
 
 
-  const db =  [
-      {id: 1, name: 'Jack Nicholson', image: 'https://nudelmania.vteximg.com.br/arquivos/ids/187520-1000-1000/Caricatura-Preto---Branco---somente-Face---1-pessoa.jpg?v=637436337593070000'},
-      {id: 2, name: 'Michael Jordan', image: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY2Njc5NDYzOTQ4NDYxNDA4/michael-jordan.jpg'},
-      {id: 3, name: 'Steven Spielberg', image: 'https://image.tmdb.org/t/p/w500/tZxcg19YQ3e8fJ0pOs7hjlnmmr6.jpg'},
-      {id: 4, name: 'Chewbacca', image: 'https://lumiere-a.akamaihd.net/v1/images/5e94826f7d7499000120d564-image_f9b9d30e.jpeg?region=0%2C48%2C1536%2C768&width=960'},
-      {id: 5, name: 'Bruce Dickinson', image: 'https://www.mesaartscenter.com/sysimg/main-image-shows-performing-live-bruce-dickinson-media-box-image-1-image.jpg'},
-      {id: 6, name: 'Eduardo Cesconetto', image: 'https://media-exp1.licdn.com/dms/image/C5603AQEeD8y12M9gEA/profile-displayphoto-shrink_200_200/0/1658772472251?e=1671062400&v=beta&t=liwzekkO3CPQLIcsULENoUA_3lqBNRFg9lFrwVE_EPg'},
+  const db = [
+    { id: 1, name: 'Jack Nicholson', image: 'https://nudelmania.vteximg.com.br/arquivos/ids/187520-1000-1000/Caricatura-Preto---Branco---somente-Face---1-pessoa.jpg?v=637436337593070000' },
+    { id: 2, name: 'Michael Jordan', image: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY2Njc5NDYzOTQ4NDYxNDA4/michael-jordan.jpg' },
+    { id: 3, name: 'Steven Spielberg', image: 'https://image.tmdb.org/t/p/w500/tZxcg19YQ3e8fJ0pOs7hjlnmmr6.jpg' },
+    { id: 4, name: 'Chewbacca', image: 'https://lumiere-a.akamaihd.net/v1/images/5e94826f7d7499000120d564-image_f9b9d30e.jpeg?region=0%2C48%2C1536%2C768&width=960' },
+    { id: 5, name: 'Bruce Dickinson', image: 'https://www.mesaartscenter.com/sysimg/main-image-shows-performing-live-bruce-dickinson-media-box-image-1-image.jpg' },
+    { id: 6, name: 'Eduardo Cesconetto', image: 'https://media-exp1.licdn.com/dms/image/C5603AQEeD8y12M9gEA/profile-displayphoto-shrink_200_200/0/1658772472251?e=1671062400&v=beta&t=liwzekkO3CPQLIcsULENoUA_3lqBNRFg9lFrwVE_EPg' },
   ]
-  
 
 
-  const click = (id) =>{
-     console.log('HEREEE');
+
+  const click = (id) => {
+    console.log('HEREEE');
   }
 
-  
+
 
   return (
 
@@ -27,16 +27,17 @@ function ListAllContact() {
       <Wrapper>
         <h1>List of All Contacts</h1>
         {db.map((d) =>
-        <div className='list' key={d.id} onClick={click}>
+          <div className='list' key={d.id} onClick={click}>
             <Avatar
               alt="contact photo"
               src={d.image}
               size="85"
               round={true}
+              className="cover"
             />
-            
-          <div className='nick'>{d.name}</div>
-        </div>
+
+            <div className='nick'>{d.name}</div>
+          </div>
         )}
       </Wrapper>
     </>
@@ -81,5 +82,8 @@ h1{
   border-radius: 0.5rem;
   height: 3vh;
 }
-
+.cover{
+  object-fit: cover;
+   width: 100%;
+}
 `
