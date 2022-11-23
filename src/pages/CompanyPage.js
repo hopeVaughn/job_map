a8db08013475e06096e8de6941da246b70644834
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-23 13:28:37 -0800

    changes to nav bar

--- src/pages/CompanyPage.js
+++ src/pages/CompanyPage.js
@@ -1,34 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import { useParams } from 'react-router-dom';
+import { companies, applications } from '../util/constants'
 
 function CompanyPage(props) {
   const [company, setCompany] = useState({})
-   
+
   // take the id   
-  let {id} = useParams();
-  
- 
+  let { id } = useParams();
+
+
 
   // get all information about the company by id
-  async function getCompany () {
+  async function getCompany() {
     const result = await axios.get(`http://localhost:8080/api/companies/${id}`)
-    
+
     setCompany(result.data[0]) //if backend send just a object, maybe is better 
   }
 
   useEffect(() => {
-    getCompany()      
+    getCompany()
   }, []);
 
 
   return (
     <>
       <div>
-          <h1>CompanyPage</h1> 
-          <h3>  Just Exemplos, can delete the div </h3>
-      ___  {company.name}   
-      ___  {company.user_id}
+        <h1>CompanyPage</h1>
+        <h3>  Just Exemplos, can delete the div </h3>
+        ___  {company.name}
+        ___  {company.user_id}
       </div>
     </>
   )
