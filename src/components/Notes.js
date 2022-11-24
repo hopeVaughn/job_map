c6d5af8db4135604ddf7ae9ee28d380ab942afc7
NadiaPia <206i417@gmail.com>
2022-11-23 22:27:03 -0800

    add and save button functionality added

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 function Notes() {
-  const fakeNotes = [    
-    {timestamp: 1669262514, text: "No need to run"},
-    {timestamp: 1669262915, text: "and hide,"},
-    {timestamp: 1669272516, text: "it's a wonderful,"},
-    {timestamp: 1669362514, text: "wonderful life"},
-    {timestamp: 1669262514, text: "No need to run"},
-    {timestamp: 1669262915, text: "and hide,"},
-    {timestamp: 1669272516, text: "it's a wonderful,"},
-    {timestamp: 1669362514, text: "wonderful life"},
+  const fakeNotes = [
+    {
+      timestamp: 1669262514,
+      text: "No need to run No need to runNo need to runNo need to runNo need to runNo need to runNo need to runNo need to runNo need to run No need to runNo need to runNo need to runNo need to runNo need to runNo need to runNo need to runNo need to run",
+    },
+    { timestamp: 1669262915, text: "and hide," },
+    { timestamp: 1669272516, text: "it's a wonderful," },
+    { timestamp: 1669362514, text: "wonderful life" },
+    { timestamp: 1669262514, text: "No need to run" },
+    { timestamp: 1669262915, text: "and hide," },
+    { timestamp: 1669272516, text: "it's a wonderful," },
+    { timestamp: 1669362514, text: "wonderful life" },
   ];
 
+  const [notes, setNotes] = useState(fakeNotes);
   const [showAdd, setShowAdd] = useState(false);
+  const [newNote, setNewNote] = useState("");
+
+  const saveClicked = () => {
+
+    setShowAdd(false);
+    if (!newNote) return;
+    setNotes([{timestamp: Date.now(), text: newNote}, ...notes]);
+    setNewNote("");
+  };
+
+  const addNewNoteClicked = () => {
+    setShowAdd(true)
+  }
 
   return (
     <Wrapper>
@@ -22,19 +38,37 @@ function Notes() {
         <h5>Historic</h5>
 
         <div className="notes-container">
-          {showAdd && <input type="text" id="typeText" className="form-control input" placeholder="Add Note"/>}
-          {fakeNotes.map((note) =>
-            <div className="note">
-              <span className="note-date">{new Date(note.timestamp).toISOString().split('T')[0]}</span>
-              <span>{note.text}</span>
-            </div> 
+          {showAdd && (
+            <div className="note new-note">
+              <span className="note-date">
+                {new Date().toISOString().split("T")[0]}
+              </span>
+              <input
+                type="text"
+                id="typeText"
+                className="input"
+                placeholder="Add Note"
+                onChange={(event) => setNewNote(event.target.value)}
+              />
+            </div>
           )}
+
+          {notes.map((note, key) => (
+            <div className="note" key={key}>
+              <span className="note-date">
+                {new Date(note.timestamp).toISOString().split("T")[0]}
+              </span>
+              <span>{note.text}</span>
+            </div>
+          ))}
         </div>
 
-        <button 
-          className="btn"
-          onClick={() => setShowAdd(!showAdd)}
-        >{showAdd ? 'Save Note' : 'Add New Note'}</button>
+        <button className="btn"
+          onClick={() => 
+            showAdd ? saveClicked() : addNewNoteClicked()
+          }>
+          {showAdd ? "Save Note"  : "Add New Note"}
+        </button>
       </div>
     </Wrapper>
   );
@@ -60,6 +94,11 @@ const Wrapper = styled.div`
 
 .input {
   width: 100%;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  color: white;
+
 }
 
 .notes-component {
@@ -71,22 +110,31 @@ const Wrapper = styled.div`
 
 .note {
   width: 100%;
-  background-color: #69822f;
-  color: white;
+  background-color: #c5d0e6;
+  color: black;
   margin: 2px 0;
   border-radius: 5px;
   display: flex;
-  flex-direction: row; 
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
 
 }
 
 .note-date {
-  background-color: #854803;
+  background-color: #ae88d1;
   border-radius: 10px;
   margin: 2px 7px;
   padding 0 5px;
+  width: 6em;
+  min-width: 6em;
+  height: 1.6em;
 }
 
-`
+.new-note {
+  background-color: #b1b7c4;
+}
+
+`;
 
 export default Notes;
