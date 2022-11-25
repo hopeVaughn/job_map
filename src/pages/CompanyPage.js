56be2edf6453325d876cdc7abb62ba2b73524708
Eduardo <cesconettoedu@gmail.com>
2022-11-24 23:08:50 -0500

    conect allcontacts to singlecontact

--- src/pages/CompanyPage.js
+++ src/pages/CompanyPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import { useParams } from 'react-router-dom';
-import { companies, applications, contacts } from '../util/constants'
+//import { companies, applications, contacts } from '../util/constants'
 import { Networks, Notes, Stages } from '../components';
 import styled from 'styled-components'
 function CompanyPage(props) {
@@ -15,7 +15,7 @@ function CompanyPage(props) {
   // get all information about the company by id
   async function getCompany() {
     const response = await axios.get(`http://localhost:8080/api/applications/custom/${id}`)
-    console.log(response.data[0]);
+    // console.log(response.data[0]);
     setCompany(response.data[0]) //if backend send just a object, maybe is better 
   }
 
