9a836e90e2742d1bf6566d1b2fea144c5c6073bb
Eduardo <cesconettoedu@gmail.com>
2022-11-28 16:28:55 -0500

    change the path in app.js contacts and singlecontact to better understand

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home, LandingPage, ListedCompanies, ApplicationPage, Register, Network, SingleNetwork, Create, CreateContact, CreateApplication } from './pages';
+import { Home, LandingPage, ListedCompanies, ApplicationPage, Register, Contacts, SingleContact, Create, CreateContact, CreateApplication } from './pages';
 
 import { useState } from "react";
 
@@ -19,8 +19,8 @@ function App() {
         <Route path='application/:id' element={<ApplicationPage />} />
         <Route path='create' element={<Create />} />
         <Route path='create/:id' element={<CreateApplication />} />
-        <Route path='contacts' element={<Network />} />
-        <Route path='contacts/:id' element={<SingleNetwork />} />
+        <Route path='contacts' element={<Contacts />} />
+        <Route path='contacts/:id' element={<SingleContact />} />
         <Route path='contact' element={<CreateContact />} />
       </Routes>
     </Router>
